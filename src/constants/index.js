c3b03b46b4d99f422810b243f1be23f11362b501
NikitaYewale007 <yewalen04@gmail.com>
2024-08-22 23:29:33 +0530

    Add initial project files

---
+++ src/constants/index.js
@@ -0,0 +1,57 @@
+import education1 from "../assets/education/education-1.jpg";
+import education2 from "../assets/education/education-2.jpg";
+
+
+export const HERO_CONTENT = `Welcome to my portfolio! I'm the Founder, CTO, and Managing Director of Sharada Productions and Media Management, where innovation meets creativity. As an engineer with a deep passion for AI and machine learning, I thrive at the intersection of technology and artistry. My journey as an entrepreneur led me to establish an arts, media, and marketing company that champions cutting-edge solutions and creative excellence. Explore my work and discover how technology and creativity come together to shape the future.`;
+
+export const ABOUT_TEXT = `I am the Founder, CTO, and Managing Director of Sharada Productions and Media Management, where I lead innovative projects at the intersection of technology and creativity. Since June 2024, I've been spearheading initiatives to connect clients with groundbreaking ideas, while building a strong network of artists and tech experts. My journey includes a recent Data Science internship at Unified Mentors, where I developed projects like a blood pressure monitoring system and applied machine learning to a bird dataset. Prior to that, I honed my skills during a Web Development internship at Crowd, designing user interfaces, coding in HTML, XML, and JavaScript, and testing web applications. Explore my work to see how I integrate my diverse experiences to craft exceptional digital solutions and drive innovation.`
+export const EXPERIENCES = [
+  {
+    year: "Jun 2024 - Present",
+    role: "Founder CTO MD Tech ",
+    company: "Sharada Productions & Media Management",
+    description:`My job is to find client and push ideas to company with paying salary and bringing network of artists and technology.`,
+    technologies: ["Leadership", "Team Management", "Innovative Mindset"],
+  },
+  {
+    year: "Apr 2024 - May 2024",
+    role: "Data Science Intern",
+    company: "UNIFIED MENTOR ",
+    description: `Worked On Two data Science Projects from Scratch To deployment.1. Blood Pressure Monitoring With The Help Of Data Science.2. Implementing data Science and Machine learning tasks on Birds Dataset.`,
+    technologies: ["R", "Python", "NoSQL", "SQL" ,"Machine learning"],
+  },
+  {
+    year: "Mar 2023 - Apr 2023 ",
+    role: "Web Development Intern",
+    company: "Crowd",
+    description:`Desideged user interfaces and navigation menus.Writing and reviewing code for sites, typically HTML, XML, or JavaScript.Testing web applications.`,
+    technologies: ["Tech Career Skills", "HTML", "Web Testing"],
+  },
+];
+
+export const EDUCATION = [
+  {
+    title: "Savitribai Phule Pune University",
+    image: education1,
+    description:"Bachelor of Engineering - Computer Engineering",
+    technologies: ["Python", "Machine Learning", "Deep Learning", "Artificial Intelligence", "NLP"],
+  },
+  {
+    title: "Cambridge VoIP Ltd",
+    image: education2,
+    description:
+      "SSC Maharashtra Board (97.60 in mathematics)",
+    technologies: ["Mathematics", "English", "Science"],
+  },
+];
+
+export const CONTACT = {
+  address: "Haveli, Maharashtra, India",
+  phoneNo: "+12 5678 6696 00 ",
+  email: "sharadaprodtech@gmail.com",
+};
+
+
+
+
+2012 - 2018
