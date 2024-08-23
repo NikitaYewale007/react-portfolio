1eba1ee47891ef48866c334874dd46ed4d6c8890
NikitaYewale007 <yewalen04@gmail.com>
2024-08-23 12:16:40 +0530

    Add Initial Files

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
       <img  className="mx-2 w-10" src={logo} alt="logo" />
     </div>
     <div className="m-8 flex items-center justify-center gap-4 text-2xl">
-      <FaLinkedin />
+     <a href="https://www.linkedin.com/in/somesh-panchal-4b0427155/" target="blank"><FaLinkedin /></a> 
       <FaGithub />
       <FaInstagram />
       <FaSquareXTwitter />
